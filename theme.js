6b630a609314656631d164608e34e8febf089c2a
James Tobey <james.tobey@shipwell.com>
2021-02-25 22:24:07 -0600

    thats a nice deck

---
+++ theme.js
@@ -0,0 +1,20 @@
+import { condensed } from "@mdx-deck/themes";
+
+export default {
+  ...condensed,
+  styles: {
+    a: {
+      color: "#006BA2",
+    },
+  },
+  colors: {
+    text: "#32CD32",
+    background: "#26282B",
+    primary: "#006BA2",
+  },
+  fonts: {
+    body: "Montserrat",
+  },
+  googleFont:
+    "https://fonts.googleapis.com/icon?family=Montserrat:300,400,500,700",
+};
